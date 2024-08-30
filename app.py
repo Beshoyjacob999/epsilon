b39a4ba39269099b39821c9e2d8b7ffb6f1324da
Beshoy jacop <b.j01289326346@gmail.com>
2024-08-30 08:02:03 +0300

    Create the main application window

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 #This is my python project
+import tkinter as tk
+root = tk.Tk()
+root.title("calculator")
+
+result_label = tk.Label(root, text="", width=20, font=("Arial", 24), borderwidth=2, relief="solid")
+result_label.grid(row=0, column=0, columnspan=4)
+
+root.mainloop()
