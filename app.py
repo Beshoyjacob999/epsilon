5ffe615f3cb88785dca11ff074bbf5a86e908c10
Beshoy jacop <b.j01289326346@gmail.com>
2024-08-30 08:04:10 +0300

    Function to update text in Label

--- app.py
+++ app.py
@@ -6,4 +6,8 @@ root.title("calculator")
 result_label = tk.Label(root, text="", width=20, font=("Arial", 24), borderwidth=2, relief="solid")
 result_label.grid(row=0, column=0, columnspan=4)
 
+def update_label(item):
+    current_text = result_label.cget("text")
+    result_label.config(text=current_text + item)
+
 root.mainloop()
